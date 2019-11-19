65927ebd3e9a67263d58023652af16601634e112
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-19 10:40:26 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_06.List_Employees_Working_at_a_Company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.1-06.List_Employees_Working_at_a_Company
